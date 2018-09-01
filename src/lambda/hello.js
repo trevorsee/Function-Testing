f8a2c8620cf4492c5967683f2c042ded1e93b692
trevorsee <tcarr@mica.edu>
2018-09-01 14:44:26 -0500

    remove geo

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -3,14 +3,13 @@ var geoip = require("geoip-lite");
 export function handler(event, context, callback) {
   console.log(event);
   const ip = event.headers["client-ip"];
-  const geo = geoip.lookup(ip);
+  // const geo = geoip.lookup(ip);
 
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
       headers: event.headers,
       ip: ip,
-      geo: geo,
       msg: "Hello, World!"
     })
   });
