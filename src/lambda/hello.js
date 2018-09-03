7e62a70db6b815830644ee6bed49bec10dfb7773
trevorsee <tcarr@mica.edu>
2018-09-03 18:46:25 -0500

    try import

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,18 +1,33 @@
-var geoip = require("geoip-lite");
+// var geoip = require("geoip-lite");
 
-export function handler(event, context, callback) {
-  console.log(event);
-  const ip = event.headers["client-ip"];
-  const geo = geoip.lookup(ip);
-  console.log(geo);
+// export function handler(event, context, callback) {
+//   console.log(event);
+//   const ip = event.headers["client-ip"];
+//   const geo = geoip.lookup(ip);
+//   console.log(geo);
 
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      headers: event.headers,
-      ip: ip,
-      geo: "geo",
-      msg: "Hello, World!"
-    })
-  });
-}
+//   callback(null, {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       headers: event.headers,
+//       ip: ip,
+//       geo: "geo",
+//       msg: "Hello, World!"
+//     })
+//   });
+// }
+
+import fetch from "node-fetch";
+
+const API_ENDPOINT =
+  "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT)
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: `${data.setup} ${data.punchline} *BA DUM TSSS*`
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
